76e5e526f310f8d97d1d5c09d90dc3e4b8790823
TechaWay <12345>
2017-01-26 02:26:34 +0200

    .

--- server/server/config/middlewares/authorization.js
+++ server/server/config/middlewares/authorization.js
@@ -2,7 +2,7 @@ var _ = require('lodash');
 
 exports.requiresLogin = function(isPhotographer){
     return function(req, res, next) {
-        if (!req.isAuthenticated() && (!isPhotographer || (isPhotographer && req.user.isPhotographer))) {
+        if (!req.isAuthenticated() && (!isPhotographer || (isPhotographer && req.user && req.user.isPhotographer))) {
             return res.status(401).send('User is not authorized');
         }
         next();
